9c2646612420f87fd21efbca654ba22da218eff2
Nguyen Tan Anh <tananh691@gmail.com>
2025-11-16 06:18:21 +0200

    feat(be): temp allow all cors

--- backend/src/main/java/com/unfinitas/backend/config/CorsConfig.java
+++ backend/src/main/java/com/unfinitas/backend/config/CorsConfig.java
@@ -6,24 +6,22 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
-import java.util.Arrays;
-import java.util.List;
-
 @Configuration
 public class CorsConfig {
 
     @Bean
     public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
+        final CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowCredentials(true);
-        config.setAllowedOriginPatterns(Arrays.asList("http://localhost:3000", "http://127.0.0.1:3000"));
-        config.setAllowedHeaders(List.of("*"));
-        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
+        config.addAllowedOriginPattern("*");   // allow all domains
+        config.addAllowedHeader("*");          // allow all headers
+        config.addAllowedMethod("*");          // allow all HTTP methods
         config.setMaxAge(3600L);
 
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
+
         return new CorsFilter(source);
     }
 }
